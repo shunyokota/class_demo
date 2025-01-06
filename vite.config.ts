30d42bfdc4e2affaf277f28e4411c55db39432fa
shunyokota <shunyokota@gmail.com>
2025-01-06 11:18:06 +0900

    sentry map

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "mimmy-inc-uf",
+    project: "javascript"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
